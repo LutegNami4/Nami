d113a5d13462496d4d64572a8cf9482fc9c093ee
Gaa-kun <42699186+lutegglh@users.noreply.github.com>
2022-09-14 14:14:13 +0700

    Update __main__.py

--- nezuko/__main__.py
+++ nezuko/__main__.py
@@ -159,7 +159,7 @@ keyboard = InlineKeyboardMarkup(
 async def start(_, message):
     if message.chat.type != enums.ChatType.PRIVATE:
         return await message.reply_photo(
-            photo="https://cdn.awwni.me/2gj9h.jpg",
+            photo="https://drive.mirror-gan.my.id/0:/-2147483648_-210348.jpg",
             caption="Pm Me For More Details.",
             reply_markup=keyboard,
         )
@@ -184,7 +184,7 @@ async def start(_, message):
             )
     else:
         await message.reply_photo(
-            photo="https://cdn.awwni.me/2gj9h.jpg",
+            photo="https://drive.mirror-gan.my.id/0:/-2147483648_-210348.jpg",
             caption=home_text_pm,
             reply_markup=home_keyboard_pm,
         )
